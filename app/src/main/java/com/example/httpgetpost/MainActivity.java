6e07760689d236036864c21d18a73198771000e6
ericsu0630 <ericsu0630@gmail.com>
2020-10-16 09:22:15 +0800

    second commit

--- app/src/main/java/com/example/httpgetpost/MainActivity.java
+++ app/src/main/java/com/example/httpgetpost/MainActivity.java
@@ -6,15 +6,14 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
-
 import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
     TextView textView;
@@ -34,11 +33,11 @@ public class MainActivity extends AppCompatActivity {
                         runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
-                                String price;
-                                DownloadJson dl = new DownloadJson();
+                                String queueNumber;
+                                GetQueueNumberAsyncTask dl = new GetQueueNumberAsyncTask();
                                 try{
-                                    price = dl.execute("https://lineup.blueeyes.com.tw/counter.php?k=7b2007112425a79e08498ac36ff76f948945890c").get();
-                                    textView.setText(price);
+                                    queueNumber = dl.execute("https://lineup.blueeyes.com.tw/counter.php?k=7b2007112425a79e08498ac36ff76f948945890c").get();
+                                    textView.setText(queueNumber);
                                 }catch(Exception e){
                                     e.printStackTrace();
                                 }
@@ -53,48 +52,48 @@ public class MainActivity extends AppCompatActivity {
         thread.start();
     }
 
-    public class DownloadJson extends AsyncTask<String, Void, String> {
+    public class GetQueueNumberAsyncTask extends AsyncTask<String, Void, String> {
 
         @Override
         protected String doInBackground(String... strings) {
-            String p;
+            String returnValue;
             try{
                 URL url = new URL(strings[0]);
-                HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                HttpsURLConnection con = (HttpsURLConnection) url.openConnection();
                 con.setRequestMethod("POST");
 
                 // For POST only - START
                 con.setDoOutput(true);
-                OutputStream os = con.getOutputStream();
+                DataOutputStream os = new DataOutputStream(con.getOutputStream());
                 os.write("key=get".getBytes());
                 os.flush();
                 os.close();
                 // For POST only - END
 
-                int responseCode = con.getResponseCode();
+                //For POST request
+                int responseCode = con.getResponseCode(); //get response code
                 Log.i("POST Response Code",String.valueOf(responseCode));
 
                 if (responseCode == HttpURLConnection.HTTP_OK) { //success
                     BufferedReader in = new BufferedReader(new InputStreamReader(
                             con.getInputStream()));
                     String inputLine;
-                    StringBuilder response = new StringBuilder();
+                    StringBuilder response = new StringBuilder(); //get POST response
 
                     while ((inputLine = in.readLine()) != null) {
                         response.append(inputLine);
                     }
                     in.close();
 
-                    // print result
-
-                    return String.valueOf(response);
+                    returnValue = String.valueOf(response);
                 } else {
-                    return "POST request not worked";
+                    returnValue = "POST request failed";
                 }
             }catch(Exception e){
                 e.printStackTrace();
-                return "failed";
+                returnValue = "URL Connection failed";
             }
+            return returnValue;
         }
     }
 }
